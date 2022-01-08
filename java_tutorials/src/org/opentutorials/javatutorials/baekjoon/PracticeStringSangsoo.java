d3f39d3d2b14cc27fcae77e0bbcd9bfbd632568d
columbooster <kinglove0403@gmail.com>
2022-01-09 00:03:16 +0900

    PracticeStringSangsoo

---
+++ java_tutorials/src/org/opentutorials/javatutorials/baekjoon/PracticeStringSangsoo.java
@@ -0,0 +1,36 @@
+package org.opentutorials.javatutorials.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class PracticeStringSangsoo {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		String A = st.nextToken();
+		String B = st.nextToken();
+		
+		StringBuffer sb1 = new StringBuffer(A);
+		StringBuffer sb2 = new StringBuffer(B);
+		
+		String reverseA = sb1.reverse().toString();
+		String reverseB = sb2.reverse().toString();
+		
+		int a = Integer.parseInt(reverseA);
+		int b = Integer.parseInt(reverseB);
+		
+		if(a > b) {
+			System.out.print(a);
+		} else {
+			System.out.print(b);
+		}
+
+	}
+
+}
