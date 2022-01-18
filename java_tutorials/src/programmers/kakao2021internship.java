f88122ab1ed3899b556eaae92166f8dde54f184a
columbooster <kinglove0403@gmail.com>
2022-01-18 15:51:10 +0900

    kakao 2021

--- java_tutorials/src/programmers/kakao2021internship.java
+++ java_tutorials/src/programmers/kakao2021internship.java
@@ -1,9 +1,35 @@
 package programmers;
 
+import java.util.HashMap;
+
+
 public class kakao2021internship {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		    public int solution(String s) {
+		        int answer = 0;
+		        
+		        HashMap<String, Integer> map = new HashMap<String, Integer>();
+		        
+		        map.put("zero",0);
+		        map.put("one",1);
+		        map.put("two",2);
+		        map.put("three",3);
+		        map.put("four",4);
+		        map.put("five",5);
+		        map.put("six",6);
+		        map.put("seven",7);
+		        map.put("eight",8);
+		        map.put("nine",9);
+		        
+		        for(String num : map.keySet()) {
+			        	if(s.contains(num)) {
+			        		s = s.replace(num, ""+map.get(num));
+			        	}
+			        }
+		        
+		        answer = Integer.parseInt(s);
+		        
+		        return answer;
 
 	}
 
