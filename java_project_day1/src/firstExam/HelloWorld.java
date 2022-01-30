5e513c0228d2392416962719a1079cfa464a66a8
columbooster <kinglove0403@gmail.com>
2022-01-30 17:17:34 +0900

    efe

---
+++ java_project_day1/src/firstExam/HelloWorld.java
@@ -0,0 +1,9 @@
+package firstExam;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("이름 : 백현규 주소 : 경기도 광주");
+		System.out.println("각오 : 화이팅!!");
+	}
+
+}
