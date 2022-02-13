0ce20ef8836235f3f3c372b9b7c2a3286f016edc
columbooster <kinglove0403@gmail.com>
2022-02-13 14:20:56 +0900

    practice instance

--- java_project_day11/src/exam_scoreOX3/ScoreOX.java
+++ java_project_day11/src/exam_scoreOX3/ScoreOX.java
@@ -1,43 +1,84 @@
 package exam_scoreOX3;
 
 public class ScoreOX {
-	
-	private int no; //번호
-	private String name; //이름
-	private int[] dap; //답
-	private int cnt; //답개수 체크 카운트
-	private int rank; //순위
-	private char[] c_ox; //ox표시
-	public static int tot; //전체 인원의 답개수 체크 총합산
-	
+
+	private int no; // 번호
+	private String name; // 이름
+	private int[] dap; // 답
+	private int cnt; // 답개수 체크 카운트
+	private int score; // 점수
+	private int rank; // 순위
+	private char[] c_ox; // ox표시
+	public static int tot; // 전체 인원의 답개수 체크 총합산
+
 	public ScoreOX() {
-		
+
 	}
-	
+
 	public ScoreOX(int no, String name, int[] dap) {
 		this.no = no;
 		this.name = name;
 		this.dap = dap;
 	}
-	
+
 	public void compute() {
-		for(int i =0; i < dap.length; i++) {
-			if(dap[i]==1) {
+		c_ox = new char[5];
+		for (int i = 0; i < dap.length; i++) {
+			if (dap[i] == 1) {
+				score += 20;
 				c_ox[i] = 'O';
-			}else {
-				c_ox[i]='X';
+				cnt++;
+				tot++;
+			} else {
+				c_ox[i] = 'X';
 			}
 		}
-		
-		
+
 	}
-	
+
 	public void display() {
-		
+		System.out.print("  "+no + "\t" + name + "\t");
+		for (int i = 0; i < c_ox.length; i++) {
+			System.out.print(c_ox[i]+"\t");
+		}
+		System.out.println(score + "\t"+ rank);
+
 	}
-	
-	public static void ranking() {
+
+	public static void ranking(ScoreOX[] sc) {
+//		int[] rank = {1,1,1,1,1};
+//		for(int i = 0; i < sc.length; i++) {
+//			sc[i].compute();
+//			rank[i]= 1;
+//			for(int j=0; j < sc.length; j++) {
+//				if(sc[i].score<sc[j].score) {
+//					rank[i]++;
+//				}
+//			}
+//		}
+//		for(int i = 0; i < sc.length; i++) {
+//			System.out.print(rank[i]);
+//		}
 		
+		//사실 더 좋은 방법은, 인자로 받은 sc배열을 오름차순 정렬을 하는 것
+		//sc배열은 각 학생의 데이터를 가지고 있으므로, 
+		//sc배열인자의 score를 기준으로 오름차순 정렬을 함 (collection 이용하면 쉬움)
+		//순서대로 ranking을 부여하면 됨.
+		//아래 로직이 결국 버블 소팅 하는 것임
+		
+		for(int i = 0 ; i < sc.length; i++)
+		{
+			int rank = 1;
+			for(int j = 0 ; j < sc.length; j++)
+			{
+				if(sc[i].score < sc[j].score)
+					rank++;
+			}
+			sc[i].rank = rank;
+		}
+		/*for (int i = 0; i < sc.length; i++) {
+			System.out.println(sc[i].rank);
+		}*/
 	}
 
 }
--- java_project_day11/src/exam_scoreOX3/ScoreOXMain.java
+++ java_project_day11/src/exam_scoreOX3/ScoreOXMain.java
@@ -4,12 +4,47 @@ public class ScoreOXMain {
 
 	public static void main(String[] args) {
 
+//		ScoreOX[] sc = new ScoreOX[] {new ScoreOX(1, "홍길동", new int[] {1,2,1,3,4}),new ScoreOX(2, "김철수", new int[] {1,1,1,1,1}),
+//				new ScoreOX(3, "이진희", new int[] {1,2,1,3,1}), new ScoreOX(4, "조현민", new int[] {1,1,1,3,1}),new ScoreOX(5, "최현정", new int[] {1,4,2,5,4})
+//		};
+		
 		ScoreOX s1 = new ScoreOX(1, "홍길동", new int[] {1,2,1,3,4});
 		ScoreOX s2 = new ScoreOX(2, "김철수", new int[] {1,1,1,1,1});
 		ScoreOX s3 = new ScoreOX(3, "이진희", new int[] {1,2,1,3,1});
 		ScoreOX s4 = new ScoreOX(4, "조현민", new int[] {1,1,1,3,1});
 		ScoreOX s5 = new ScoreOX(5, "최현정", new int[] {1,4,2,5,4});
 		
+		ScoreOX[] sc = new ScoreOX[5];
+		sc[0] = s1; sc[1] = s2; sc[2] = s3; sc[3] = s4; sc[4] = s5;
+		
+		System.out.println("  * * 시험결과 * *");
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println("번호\t"+"이름\t"+"1\t"+"2\t"+"3\t"+"4\t"+"5\t"+ "점수\t"+ "등수");
+		System.out.println("---------------------------------------------------------------------");
+		
+		
+		for(int i = 0; i < sc.length; i++) {
+			sc[i].compute();
+		}
+		
+		ScoreOX.ranking(sc);
+		
+		for(int i = 0; i < sc.length; i++) {
+			sc[i].display();
+		}
+		
+		System.out.println();
+		
+		System.out.println("전체 맞은 개수 = "+ ScoreOX.tot);
+		
+		System.out.println();
+		
+		
+		//이미 각 학생 별로 ScoreOX Instance를 할당 하였고, compute()를 수행한 결과를 재사용해야함
+		//기존 코드의 경우 ranaking함수 안에서 재 수행하게 되므로 비효율적이고,
+		//static int tot값도 변경되게 됨
+
+
 		
 	}
 
