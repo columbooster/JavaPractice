ae91ef9bb2ef8ca0c9f912506998445c10d0c113
columbooster <kinglove0403@gmail.com>
2022-03-20 19:42:44 +0900

    Algorithm

---
+++ Practice_coding/src/example/Baekjoon10926.java
@@ -0,0 +1,19 @@
+package example;
+
+import java.util.Scanner;
+
+public class Baekjoon10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input;
+		
+		input = sc.next();
+		
+		System.out.println(input+"??!");
+		
+	}
+
+}
