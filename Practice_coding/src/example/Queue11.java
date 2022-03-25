b4d30d316b32b61e4b4f9ff9f21e5746c8269478
columbooster <kinglove0403@gmail.com>
2022-03-25 15:24:39 +0900

    sdsds

--- Practice_coding/src/example/Queue11.java
+++ Practice_coding/src/example/Queue11.java
@@ -1,18 +1,60 @@
 package example;
 
-import java.util.LinkedList; //import
-import java.util.Queue; //import
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Queue11 {
 
-	public static void main(String[] args) {
-	
-		Queue<Integer> queue = new LinkedList<>(); 
+	public static void main(String[] args) throws java.lang.Exception {
 
-		Queue<Integer> stack = new LinkedList<>(); //int형 queue 선언
-		queue.add(1);     // queue에 값 1 추가
-		queue.add(2);     // queue에 값 2 추가
-		queue.offer(3);   // queue에 값 3 추가
+		Stack<Integer> stk = new Stack<>();
+
+		if (stk.empty()) {
+			stk.push(1);
+			stk.push(2);
+			stk.push(3);
+		}
+
+		if (!stk.empty()) {
+			if (stk.peek() == 3)
+				stk.pop();
+
+		}
+
+		if (stk.search(3) == -1) {
+			System.out.println("3 is poped");
+		}
+
+		HashMap<String, Integer> hm = new HashMap<>();
+
+		hm.put("key1", 1);
+		hm.put("key2", 2);
+		hm.put("key3", 3);
+
+		if (hm.containsKey("key1") && hm.containsValue(1))
+			System.out.println("YES");
+
+		hm.put("key1", hm.getOrDefault(("key1"), 0) * 10);
+
+		List<String> keyList = new ArrayList<>(hm.keySet());
+
+		// 오름차순으로 키 정렬
+		Collections.sort(keyList, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				int v1 = hm.get(s1);
+				int v2 = hm.get(s2);
+
+				return Integer.compare(v1, v2);
+			}
+		});
+
+		// 출력
+		for (String s : keyList) {
+			System.out.println(s + " = " + hm.get(s));
+		}
+
+		System.out.println(hm.size());
 
 	}
 
