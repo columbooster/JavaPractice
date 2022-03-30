dbcfbdc0fcc2c28e22f56ab83d576d9722536e82
columbooster <kinglove0403@gmail.com>
2022-03-30 18:31:58 +0900

    tree set exam

---
+++ Practice_coding/src/example/TreeSetExam.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.util.TreeSet;
+
+public class TreeSetExam {
+
+	public static void main(String[] args) {
+		  TreeSet<String> treeSet = new TreeSet<String>();
+
+	        treeSet.add("홍길동");
+	        treeSet.add("강감찬");
+	        treeSet.add("이순신");
+
+	        for(String str : treeSet){
+	            System.out.println(str);  // 강감찬 이순신 홍길동// 오름찬순되어있음
+	                                    //String에 이미 Comparable 구현되어잇음
+	        }
+
+	}
+
+}
