03bdfcf264cf0394ff7888742d367302939d2645
columbooster <kinglove0403@gmail.com>
2022-04-03 18:34:04 +0900

    dfdf

---
+++ Practice_coding/src/example/DBTest.java
@@ -0,0 +1,55 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java_project_DB.ConnectDatabase;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		
+		addBook("Clean Code(클린 코드)", "인사이트", "2013", 33000);
+	}
+	
+	
+	private static void addBook(String title, String publisher, String year, int price) {
+		Connection con = ConnectDatabase.makeConnection("javauser", "java1234");
+		Statement stmt = null;
+
+		try {
+			stmt = con.createStatement();
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("INSERT INTO books (book_id, title, publisher, ");
+			sb.append("year, price) VALUES (books_seq.nextval, ");
+			sb.append("'" + title + "','" + publisher + "','");
+			sb.append(year + "'," + price + ")");
+
+			System.out.println("쿼리문 확인 : " + sb);
+
+			int insertCount = stmt.executeUpdate(sb.toString());
+			if (insertCount == 1)
+				System.out.println("레코드 추가 성공");
+			else
+				System.out.println("레코드 추가 실패");
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if (con != null)
+					con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+	}
+
+}
