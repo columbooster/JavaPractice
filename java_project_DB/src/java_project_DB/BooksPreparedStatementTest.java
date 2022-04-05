9b035b765e62f5b867b1492d402be8d8d0a4f58c
columbooster <kinglove0403@gmail.com>
2022-04-05 15:21:58 +0900

    db test

--- java_project_DB/src/java_project_DB/BooksPreparedStatementTest.java
+++ java_project_DB/src/java_project_DB/BooksPreparedStatementTest.java
@@ -2,6 +2,7 @@ package java_project_DB;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -104,10 +105,82 @@ public class BooksPreparedStatementTest {
 	}
 
 	private static void readAllBook() {
+		Connection con = ConnectDatabase.makeConnection("javauser", "java1234");
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			StringBuffer sb = new StringBuffer();
+			sb.append("select book_id, title, publisher, year, price ");
+			sb.append("from books order by book_id");
+
+			pstmt = con.prepareStatement(sb.toString());
+			rs = pstmt.executeQuery();
+			System.out.println("\n**** books 테이블 데이터 출력 ****");
+			System.out.printf("%s\t%-20s\t%6s\t%12s\t%s\n", "책번호", "책제목", "출판사", "출판연도", "가격");
+
+			while (rs.next()) {
+				System.out.printf("%d\t", rs.getInt("book_id"));
+				System.out.printf("%-26s", rs.getString("title"));
+				System.out.printf("%-13s\t", rs.getString("publisher"));
+				System.out.printf("%s\t", rs.getString("year"));
+				System.out.printf("%d\n", rs.getInt("price"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+			;
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 
 	}
 
 	private static void deleteBook(int book_id) {
+		Connection con = ConnectDatabase.makeConnection("javauser", "java1234");
+		PreparedStatement pstmt = null;
+
+		try {
+			StringBuffer sb = new StringBuffer();
+			sb.append("delete from books where book_id = ?");
+			pstmt = con.prepareStatement(sb.toString());
+
+			pstmt.setInt(1, book_id);
+
+			int deleteCount = pstmt.executeUpdate();
+
+			if (deleteCount == 1)
+				System.out.println("레코드 삭제 성공");
+			else
+				System.out.println("레코드 삭제 대상이 존재하지 않음");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+			;
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 
 	}
 
