75c10cbdbab0b2aac9324a5c33296022005d12b1
columbooster <kinglove0403@gmail.com>
2022-05-04 22:42:23 +0900

    TREESET

--- Practice_coding/src/example/TreeSetExam.java
+++ Practice_coding/src/example/TreeSetExam.java
@@ -11,6 +11,7 @@ public class TreeSetExam {
 	        treeSet.add("강감찬");
 	        treeSet.add("이순신");
 	        treeSet.add("정약용");
+	        treeSet.add("해냈다");
 
 	        for(String str : treeSet){
 	            System.out.println(str);  // 강감찬 이순신 홍길동// 오름찬순되어있음
