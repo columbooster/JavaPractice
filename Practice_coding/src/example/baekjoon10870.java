4aebe53c9c634e394991332e89aa09041d5c4bf7
columbooster <kinglove0403@gmail.com>
2022-05-05 19:53:16 +0900

    백준 피보나치

---
+++ Practice_coding/src/example/baekjoon10870.java
@@ -0,0 +1,28 @@
+package example;
+
+import java.util.Scanner;
+
+public class baekjoon10870 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		System.out.println(Fibonacci(n));
+		
+		
+	}
+	
+	public static int Fibonacci(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1 || n == 2)
+			return 1;
+		else
+			return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+	}
+	
+}
