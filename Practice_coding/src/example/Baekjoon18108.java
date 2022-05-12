a8d4f7e74cfba27ee94639952eb3a3b256c3a01b
columbooster <kinglove0403@gmail.com>
2022-05-12 23:00:52 +0900

    coding test

---
+++ Practice_coding/src/example/Baekjoon18108.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.util.Scanner;
+
+public class Baekjoon18108 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int bYear = input.nextInt();
+		
+		int birth = (bYear - 543);
+		
+		System.out.print(birth);
+	}
+
+}
