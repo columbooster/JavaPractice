320a96deadbb9c12faed05dd4ffb02c1f34ab358
columbooster <kinglove0403@gmail.com>
2022-05-14 21:09:55 +0900

    LinkedList ex

---
+++ Practice_coding/src/example/LinkedListEx.java
@@ -0,0 +1,19 @@
+package example;
+
+import java.util.LinkedList;
+
+public class LinkedListEx {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		
+		System.out.println(list.toString());
+		
+	}
+
+}
