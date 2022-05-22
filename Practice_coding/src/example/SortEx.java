d9c370e20f6c7826021cacb499e08a606b856ba9
columbooster <kinglove0403@gmail.com>
2022-05-22 16:53:39 +0900

    sort

---
+++ Practice_coding/src/example/SortEx.java
@@ -0,0 +1,24 @@
+package example;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortEx {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		char[] nums = input.nextLine().toCharArray();
+		
+		Arrays.sort(nums);
+		
+		for (int i = nums.length - 1; i >= 0; i--) {
+			System.out.print(nums[i]);
+		}
+		
+		
+		
+	}
+
+}
